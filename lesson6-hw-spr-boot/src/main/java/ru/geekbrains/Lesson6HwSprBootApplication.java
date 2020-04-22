5ef8e7f4a9705d5fecc60706e9c7668f0f9812ac
iourilitv <iourilitv1965@gmail.com>
2020-04-22 07:58:26 +0300

    Created draft [lesson6-hw-spr-boot] for a new module on Spring Boot.

---
+++ lesson6-hw-spr-boot/src/main/java/ru/geekbrains/Lesson6HwSprBootApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson6HwSprBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson6HwSprBootApplication.class, args);
+    }
+
+}
---
+++ lesson6-hw-spr-boot/src/test/java/ru/geekbrains/Lesson6HwSprBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson6HwSprBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
